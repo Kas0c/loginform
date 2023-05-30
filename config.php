85921161bbe4f82f353b9735e14e617a106c2726
BigKumpel <100943901+BigKumpel@users.noreply.github.com>
2023-05-30 22:15:42 +0200

    twig

--- config.php
+++ config.php
@@ -1,4 +1,11 @@
 <?php
+require_once('vendor/autoload.php');
+require_once('class/User.class.php');
+//loader to pomocnik do ładowania szablonów
+$loader = new Twig\Loader\FilesystemLoader('templates');
+//inicjacja twig'a
+$twig = new Twig\Environment($loader);
+
 $db = new mysqli('localhost', 'root', '', 'loginform');
 
 ?>
