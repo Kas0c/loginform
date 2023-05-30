008fcb0d2f3513d7b4701806cc8489c9f56ecfe0
BigKumpel <100943901+BigKumpel@users.noreply.github.com>
2023-05-30 22:27:37 +0200

    register twig

--- register.php
+++ register.php
@@ -1,36 +1,17 @@
-<!DOCTYPE html>
-<html lang="pl">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Rejestracja</title>
-</head>
-<body>
-    <form action="" method="post">
-        <label for="loginID">Login:</label><br>
-        <input type="text" name="login" id="loginID"><br>
-        <label for="passwordID">Hasło:</label><br>
-        <input type="password" name="password" id="passwordID"><br>
-        <label for="firstNameID">Imie:</label><br>
-        <input type="text" name="firstName" id="firstNameID"><br>
-        <label for="lastNameID">Nazwisko:</label><br>
-        <input type="text" name="lastName" id="lastNameID"><br>
-        <input type="submit" value="Zarejestruj">
-</form>
 <?php
+require_once('config.php');
 if(isset($_REQUEST['login']) && isset($_REQUEST['password'])) {
-    require_once('config.php');
-    require_once('class/User.class.php');
     $user = new User($_REQUEST['login'], $_REQUEST['password']);
     $user->setFirstName($_REQUEST['firstName']);
     $user->setLastName($_REQUEST['lastName']);
     if($user->register()) {
-        echo "Zarejestrowano prawidłowo";
+        $twig->display('message.html.twig',
+                     ["message" => "Zarejestrowano prawidłowo"]);
     } else {
-        echo "Błąd rejestracji";
+        $twig->display('message.html.twig',
+                     ["message" => "Błąd rejestracji"]);
     }
+} else {
+    $twig->display('register.html.twig');
 }
 ?>
-</body>
-</html>
