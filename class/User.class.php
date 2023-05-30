4d6830667223f317d8f5cda9e1436e12cc117fb7
BigKumpel <100943901+BigKumpel@users.noreply.github.com>
2023-05-30 20:47:55 +0200

    implementacja loginu

--- class/User.class.php
+++ class/User.class.php
@@ -9,6 +9,45 @@ class User {
     public function __construct(string $login, string $password) {
         $this->login = $login;
         $this->password = $password;
+        $this->firstName = "";
+        $this->lastName = "";
+    }
+
+    public function register() {
+        $passwordHash = password_hash($this->password, PASSWORD_ARGON2I);
+        $query = "INSERT INTO user VALUES (NULL, ?, ?, ?, ?)";
+        $db = new mysqli('localhost', 'root', '', 'loginform');
+        $preparedQuery = $db->prepare($query);
+        $preparedQuery->bind_param('ssss', $this->login, 
+                                            $passwordHash, 
+                                            $this->firstName, 
+                                            $this->lastName);
+        $preparedQuery->execute();
+    }
+
+    public function login() {
+        $query = "SELECT * FROM user WHERE login = ?";
+        $db = new mysqli('localhost', 'root', '', 'loginform');
+        $preparedQuery = $db->prepare($query);
+        $preparedQuery->bind_param('s', $this->login);
+        $preparedQuery->execute();
+        $result = $preparedQuery->get_result();
+        if($result->num_rows == 1) {
+            $row = $result->fetch_assoc();
+            $passwordHash = $row['password'];
+            if(password_verify($this->password, $passwordHash)) {
+                $this->id = $row['id'];
+                $this->firstName = $row['firstName'];
+                $this->lastName = $row['lastName'];
+                echo "zalogowano poprawnie";
+            } else {
+                echo "Błędne login lub hasło";
+            }
+        } else {
+            //brak zgadzających się wyników - wyjście z funkcji
+            echo "Błędne login lub hasło";
+            return;
+        }
     }
 }
 ?>
